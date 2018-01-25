b85dbe010ee812f471de2449524a4d197ba355aa
christofferjerrebo <christoffer.jerrebo@jayway.com>
2018-01-25 09:12:18 +0100

    Fixed env variables.

--- combinedgen/combinedgen.go
+++ combinedgen/combinedgen.go
@@ -46,16 +46,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func setserviceurls() {
-	namegenhost := os.Getenv("NAMEGEN_SERVICE_SERVICE_HOST")
+	namegenhost := os.Getenv("NAMEGENSERVICE_SERVICE_HOST")
 	log.Printf("namegen host: %s", namegenhost)
-	namegenport := os.Getenv("NAMEGEN_SERVICE_SERVICE_PORT")
+	namegenport := os.Getenv("NAMEGENSERVICE_SERVICE_PORT")
 	log.Printf("namegen port: %s", namegenhost)
 	namegenurl = fmt.Sprintf("http://%s:%s", namegenhost, namegenport)
 	log.Printf("namegen service url: %s", namegenurl)
 
-	numbergenhost := os.Getenv("NUMBERGEN_SERVICE_SERVICE_HOST")
+	numbergenhost := os.Getenv("NUMBERGENSERVICE_SERVICE_HOST")
 	log.Printf("numbergen host: %s", numbergenhost)
-	numbergenport := os.Getenv("NUMBERGEN_SERVICE_SERVICE_PORT")
+	numbergenport := os.Getenv("NUMBERGENSERVICE_SERVICE_PORT")
 	log.Printf("numbergen port: %s", numbergenport)
 	numbergenurl = fmt.Sprintf("http://%s:%s", numbergenhost, numbergenport)
 	log.Printf("numbergen service url: %s", numbergenurl)
