c68ad052253d382c0e5a8cecc0c9bab3c6a57a4f
christofferjerrebo <christoffer.jerrebo@jayway.com>
2018-01-31 16:16:15 +0100

    Services now logs when generating errors

--- namegen/namegen.go
+++ namegen/namegen.go
@@ -24,24 +24,30 @@ var (
 	}
 )
 var failToggle = false
+var generateErrors = false
 
 func main() {
 	http.HandleFunc("/", handler)
+	generateErrorsEnv := os.Getenv("GENERATE_ERRORS")
+	generateErrorsEnv = strings.ToLower(generateErrorsEnv)
+	if generateErrorsEnv == "true" {
+		generateErrors = true
+	}
+	log.Printf("Generate errors: %t\n", generateErrors)
 	log.Print("Listening.\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Handling request.\n")
-	envFail := os.Getenv("GENERATE_ERRORS")
-	envFail = strings.ToLower(envFail)
-	log.Printf("Fail set to: %s\n", envFail)
-	if envFail == "true" && failToggle {
+	if generateErrors && failToggle {
+		log.Print("Failing!\n")
 		http.Error(w, "Toggled error", http.StatusInternalServerError)
 	} else {
 		randsrc := rand.NewSource(time.Now().UnixNano())
 		rnd := rand.New(randsrc)
 		name := animals[rnd.Intn(len(animals))]
+		log.Print("Success.\n")
 		fmt.Fprintf(w, "%s\n", name)
 	}
 	failToggle = !failToggle
--- numbergen/numbergen.go
+++ numbergen/numbergen.go
@@ -11,23 +11,29 @@ import (
 )
 
 var failToggle = false
+var generateErrors = false
 
 func main() {
 	http.HandleFunc("/", handler)
+	generateErrorsEnv := os.Getenv("GENERATE_ERRORS")
+	generateErrorsEnv = strings.ToLower(generateErrorsEnv)
+	if generateErrorsEnv == "true" {
+		generateErrors = true
+	}
+	log.Printf("Generate errors: %t\n", generateErrors)
 	log.Print("Listening.\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Handling request.\n")
-	envFail := os.Getenv("GENERATE_ERRORS")
-	envFail = strings.ToLower(envFail)
-	log.Printf("Fail set to: %s\n", envFail)
-	if envFail == "true" && failToggle {
+	if generateErrors && failToggle {
+		log.Print("Failing!\n")
 		http.Error(w, "Toggled error", http.StatusInternalServerError)
 	} else {
 		randsrc := rand.NewSource(time.Now().UnixNano())
 		rnd := rand.New(randsrc)
+		log.Print("Success.\n")
 		fmt.Fprintf(w, "%d\n", rnd.Intn(100))
 	}
 	failToggle = !failToggle
